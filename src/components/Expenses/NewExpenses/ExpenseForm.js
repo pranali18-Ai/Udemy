6733d294eb34fb106b68303c1552a46920682dec
Pranali Patil <patilpranali0018@gmail.com>
2023-09-08 19:53:20 +0530

    8 sept (2)

---
+++ src/components/Expenses/NewExpenses/ExpenseForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './NewExpenses';
+import './ExpenseForm.css';
+
+
+const ExpenseForm = () => {
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" min='0.01' step='0.01' />
+                </div>
+            </div>
+
+            <div className="new-expense__control">
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min='2019-01-01' max='2023-12-31' />
+                </div>
+            </div>
+            
+            <div className='new-expense__actions'>
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+
+        // <form>
+        //     <div class="input-group mb-3">
+        //         <input type="text" placeholder='Title'/>
+            
+        //     </div>
+        // </form>
+        
+    );
+};
+export default ExpenseForm;
